42e2a8be14bdaad80f83cfe37998c94b8b90513a
Matthew Flatt <mflatt@racket-lang.org>
2018-05-26 09:09:24 -0600

    fix a test

--- tests/untyped-use-untyped.rkt
+++ tests/untyped-use-untyped.rkt
@@ -1,8 +1,8 @@
-#lang plai-typed/untyped
+#lang plait/untyped
 (require "untyped.rkt")
 
 (define-syntax-rule (test a b)
   (unless (equal? a b)
-    (error 'test "failed: ~.s" 'b)))
+    (error 'test "failed: ~.s" `b)))
 
-(test '(6 6) (apply-identity (lambda (x) 6) 5))
+(test `(6 6) (apply-identity (lambda (x) 6) 5))
